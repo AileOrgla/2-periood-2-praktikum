a8fbccb2f2220242326a3f38d9094fa796c22228
AileOrgla <aileorgla@gmail.com>
2014-10-10 14:36:59 +0300

    Add 2-periood-2-praktikum files

---
+++ new_page.php
@@ -0,0 +1,11 @@
+<?php require "components/site-functions.php"; ?>
+<?php redirect_to('http://www.neti.ee'); ?>
+ <!DOCTYPE html>
+	<html>
+			<head>
+				<?php include "components/html-head.php"; ?>
+			</head>
+		<body>
+  			<h1>See on teistsugune leht!</h1>
+		</body>
+	</html>
